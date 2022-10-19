478ae2148b6f0e44a5ed10d61790763dc16612f3
Kwu Hyen Kwak <kkjh9960@gmail.com>
2022-10-19 23:05:38 +0900

    1

--- src/shared/router.js
+++ src/shared/router.js
@@ -4,8 +4,6 @@ import Post from "../pages/Post";
 import Update from "../pages/Update";
 import Home from "../pages/Home";
 import List from "../pages/List";
-import Detail from "../pages/Detail";
-
 
 const Router = () => {
   return (
@@ -14,8 +12,6 @@ const Router = () => {
         <Route path="/" element={<Home />} />
         <Route path="/list" element={<List />} />
         <Route path="/post" element={<Post />} />
-        <Route path="/feeds" element={<Detail />} />
-        <Route path="/feeds/:id" element={<Detail />} />
         <Route path="/update" element={<Update />} />
         <Route path="/update/:id" element={<Update />} />
       </Routes>
